6a8cd8f893e6b493040c2bf7724eabd21038fb91
PabloCopes00 <112401413+PabloCopes00@users.noreply.github.com>
2022-11-25 19:19:12 -0300

    1 file modified, 1 added

---
+++ src/app/beer-list/Beer.ts
@@ -0,0 +1,9 @@
+export interface Beer {
+  name: string;
+  type: string;
+  abv: number;
+  ibu: number;
+  stock: number;
+  price: number;
+  imgage: string;
+}
--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Beer } from './Beer';
 
 @Component({
   selector: 'app-beer-list',
   templateUrl: './beer-list.component.html',
-  styleUrls: ['./beer-list.component.scss']
+  styleUrls: ['./beer-list.component.scss'],
 })
 export class BeerListComponent implements OnInit {
-
-  beer = {
-    "name": "New England Ipa",
-    "type": "Rubia",
-    "abvibu": "6% / 45",
-    "stock": 30,
-    "price": 600
+  beer: Beer = {
+    name: 'New England Ipa',
+    type: 'Rubia',
+    abv: 3,
+    ibu: 3,
+    stock: 30,
+    price: 600,
+    image: 'imagen',
   };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
